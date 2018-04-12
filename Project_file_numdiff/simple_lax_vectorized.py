f4d19b6034780ab45149bfb420f3942b87b67790
Julie Røste <julir@stud.ntnu.no>
2018-04-12 13:53:17 +0200

    simple vec const

--- Project_file_numdiff/simple_lax_vectorized.py
+++ Project_file_numdiff/simple_lax_vectorized.py
@@ -8,31 +8,31 @@ def f(u_last):
     f_step[:] = u_last[0]*u_last[1], (u_last[1]**2)/2 + c.C**2*np.log(u_last[0])
     return f_step
 
-def s(time, position, u_last, delta_t, delta_x, j, tau, V0, my, rho_max, E):
+def s(time, position, u_last, delta_t, delta_x, j):
     s_step = np.zeros(2)
-    s_step[:] = q_in(time)*phi(position), (1/tau)*((V0*(1-u_last[j,0]/rho_max))/(1+E*(u_last[j,0]/rho_max)**4)
-                                                         - u_last[j,1])+my*delta_t*(u_last[j+1,1]-2*u_last[j,1]
+    s_step[:] = c.q_in(time)*c.phi(position), (1/c.TAU)*((c.V0*(1-u_last[j,0]/c.RHO_MAX))/(1+c.E*(u_last[j,0]/c.RHO_MAX)**4)
+                                                         - u_last[j,1])+c.MY*delta_t*(u_last[j+1,1]-2*u_last[j,1]
                                                          + u_last[j-1,1])/(u_last[j,0]*delta_x**2)
     return s_step
 
-def u_next_simple_lax(u_last, delta_t, delta_x, j, time, position, tau, V0, my, rho_max, E):
-    return (u_last[j+1]+u_last[j-1])/2 - delta_t/(2*delta_x)*(f(u_last[j+1])-f(u_last[j-1])) + delta_t*s(time, position, u_last, delta_t, delta_x, j, tau, V0, my, rho_max, E)
+def u_next_simple_lax(u_last, delta_t, delta_x, j, time, position):
+    return (u_last[j+1]+u_last[j-1])/2 - delta_t/(2*delta_x)*(f(u_last[j+1])-f(u_last[j-1])) + delta_t*s(time, position, u_last, delta_t, delta_x, j)
 
-def one_step_simple_lax(u_last, X, delta_t, delta_x ,time, rho0, L, tau, V0, my, rho_max, E):
+def one_step_simple_lax(u_last, X, delta_t, delta_x ,time):
     u_next = np.zeros((X,2))
-    u_next[0,:] = rho0, safe_v(rho0)
+    u_next[0,:] = c.RHO_0, c.safe_v(c.RHO_0)
     for j in range(1,X-1):
-        position=j*delta_x-L/2
-        u_next[j] = u_next_simple_lax(u_last, delta_t, delta_x, j, time, position, tau, V0, my, rho_max, E)
+        position=j*delta_x-c.L/2
+        u_next[j] = u_next_simple_lax(u_last, delta_t, delta_x, j, time, position)
     u_next[X-1]=2*u_next[X-2]-u_next[X-3]
     return u_next
 
 def solve_simple_lax(T, X, delta_t, delta_x):
-    rho0, L, tau , V0, my, rho_max, E = c.RHO_0, c.L, c.TAU, c.V0, c.MY, c.RHO_MAX, c.E
-    grid_u = initialize_grid(T, X, rho0)
+
+    grid_u = c.initialize_grid(T, X, c.RHO_0)
     for i in range(1, T):
         time=i*delta_t
-        grid_u[i]=one_step_simple_lax(grid_u[i-1], X, delta_t, delta_x, time, rho0, L, tau, V0, my, rho_max, E)
+        grid_u[i]=one_step_simple_lax(grid_u[i-1], X, delta_t, delta_x, time)
     return grid_u
 
 def plot_simple_lax(T, X, delta_x, grid_u):
