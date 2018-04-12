1833254fb346307976ead060212b5661521df8a9
Julie Røste <julir@stud.ntnu.no>
2018-04-12 12:07:08 +0200

    convergence plot

--- constants.py
+++ constants.py
@@ -1,18 +1,24 @@
 import numpy as np
 
+
+
 M=8 #2^m, gives number of points in space
 MAX_TIME=5*60 #seconds
 TIME_POINTS=1000
 SPACE_POINTS=2**(M)
-V0=120
+L=5000 #meter
+
+delta_t=0.001
+delta_x=L/(SPACE_POINTS-1)  #meter
+
+V0=33.33
 RHO_MAX=140
 E=100
 RHO_0=40
-L=5000 #meter
-delta_t=0.001
-delta_x=L/(SPACE_POINTS-1)  #meter
+Q_IN=2000
 
 SIGMA=56.7
 MY=600
 TAU=0.5
 C=54
+
