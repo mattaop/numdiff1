b7661f97338b3b341befa33c6e11ce31b0c9a74e
Mathias Opland <Mathias.opland@gmail.com>
2018-04-12 12:36:38 +0200

    k

--- upwind_vectorized.py
+++ upwind_vectorized.py
@@ -22,31 +22,32 @@ def f(u_last, j):
     f_step[:] = u_last[j,0]*u_last[j,1], (u_last[j,1]**2)/2 + c.C**2*np.log(u_last[j,0])
     return f_step
 
-def s(time, position, u_last, delta_t, delta_x, j):
+def s(time, position, u_last, delta_t, delta_x, j, tau, V0, my, rho_max, E):
     s_step = np.zeros(2)
-    s_step[:] = q_in(time)*phi(position), (1/c.TAU)*((c.V0*(1-u_last[j,0]/c.RHO_MAX))/(1+c.E*(u_last[j,0]/c.RHO_MAX)**4)
-                                                         - u_last[j,1])+c.MY*delta_t*(u_last[j+1,1]-2*u_last[j,1]
+    s_step[:] = q_in(time)*phi(position), (1/tau)*((V0*(1-u_last[j,0]/rho_max))/(1+E*(u_last[j,0]/rho_max)**4)
+                                                         - u_last[j,1])+my*delta_t*(u_last[j+1,1]-2*u_last[j,1]
                                                          + u_last[j-1,1])/(u_last[j,0]*delta_x**2)
     return s_step
 
-def u_next_upwind(u_last, delta_t, delta_x, j, time, position):
-    return u_last[j] - delta_t/delta_x*(f(u_last,j)-f(u_last,j-1)) + delta_t*s(time, position, u_last, delta_t, delta_x, j)
+def u_next_upwind(u_last, delta_t, delta_x, j, time, position, tau, V0, my, rho_max, E):
+    return u_last[j] - delta_t/delta_x*(f(u_last,j)-f(u_last,j-1)) + delta_t*s(time, position, u_last, delta_t, delta_x, j, tau, V0, my, rho_max, E)
 
 
-def one_step_upwind(u_last, X, delta_t, delta_x ,time, rho0, L):
+def one_step_upwind(u_last, X, delta_t, delta_x ,time, rho0, L, tau, V0, my, rho_max, E):
     u_next = np.zeros((X,2))
     u_next[0,:] = rho0, safe_v(rho0)
     for j in range(1,X-1):
         position=j*delta_x-L/2
-        u_next[j] = u_next_upwind(u_last, delta_t, delta_x, j, time, position)
+        u_next[j] = u_next_upwind(u_last, delta_t, delta_x, j, time, position, tau, V0, my, rho_max, E)
     u_next[X-1]=2*u_next[X-2]-u_next[X-3]
     return u_next
 
 def solve_upwind(T, X, delta_t, delta_x):
-    grid_u = initialize_grid(T, X, c.RHO_0)
+    rho0, L, tau , V0, my, rho_max, E = c.RHO_0, c.L, c.TAU, c.V0, c.MY, c.RHO_MAX, c.E
+    grid_u = initialize_grid(T, X, rho0)
     for i in range(1, T):
         time=i*delta_t
-        grid_u[i]=one_step_upwind(grid_u[i-1], X, delta_t, delta_x, time, c.RHO_0, c.L)
+        grid_u[i]=one_step_upwind(grid_u[i-1], X, delta_t, delta_x, time, rho0, L, tau, V0, my, rho_max, E)
     return grid_u
 
 def plot_upwind(T, X, delta_x, grid_u):
@@ -56,9 +57,7 @@ def plot_upwind(T, X, delta_x, grid_u):
 
 
 def main():
-
     grid_u = solve_upwind(c.TIME_POINTS, c.SPACE_POINTS, c.delta_t, c.delta_x)
-
     plot_upwind(c.TIME_POINTS, c.SPACE_POINTS, c.delta_x, grid_u[:,:,0])
     plot_upwind(c.TIME_POINTS, c.SPACE_POINTS, c.delta_x, grid_u[:,:,1])
 main()
